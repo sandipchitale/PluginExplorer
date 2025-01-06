8a42e962798ecb68b4ceae0e38bc2ff28ff7d34d
Sandip Chitale <sandipchitale@gmail.com>
2025-01-05 22:24:27 -0500

    Add "class" attribute.

--- src/main/java/dev/sandipchitale/pluginexplorer/PluginXmlGotoDeclarationHandler.java
+++ src/main/java/dev/sandipchitale/pluginexplorer/PluginXmlGotoDeclarationHandler.java
@@ -24,6 +24,7 @@ public class PluginXmlGotoDeclarationHandler extends GotoDeclarationHandlerBase 
 
     private static final List<String> potentialTypeNames = List.of(
             "builderClass",
+            "class",
             "className",
             "factoryClass",
             "hostElementClass",
